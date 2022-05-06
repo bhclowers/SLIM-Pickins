2cf623fce9b7e817351f19a24a9985b5d94465f7
bhclowers <bhclowers@gmail.com>
2022-05-06 15:35:28 -0700

    Add files via upload

---
+++ KiCAD Plugin/SLIM/__init__.py
@@ -0,0 +1,15 @@
+#https://forum.kicad.info/t/howto-register-a-python-plugin-inside-pcbnew-tools-menu/5540/22
+try:
+    # Note the relative import!
+    from .slim_layout_v1 import *
+    # Instantiate and register to Pcbnew
+    # text_by_date().register()
+    moveSLIM().register()
+    moveSLIMDlg().register()
+except Exception as e:
+    import os
+    plugin_dir = os.path.dirname(os.path.realpath(__file__))
+    log_file = os.path.join(plugin_dir, 'SLIM.log')
+    with open(log_file, 'w') as f:
+        f.write(repr(e))
+
